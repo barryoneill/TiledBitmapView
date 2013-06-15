832e485af0ad1c76f8b9955ee3e090144434ca96
Barry O'Neill <barra.oneill@gmail.com>
2013-06-14 21:48:23 -0400

    apply licence

--- demo/src/net/nologin/meep/tbv/demo/DemoTileProvider.java
+++ demo/src/net/nologin/meep/tbv/demo/DemoTileProvider.java
@@ -1,3 +1,19 @@
+/*
+ *    TiledBitmapViewDemo - An Android application demonstrating the abilities of the TiledBitmapView library
+ *    Copyright 2013 Barry O'Neill (http://meep.nologin.net/)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package net.nologin.meep.tbv.demo;
 
 import android.content.Context;
